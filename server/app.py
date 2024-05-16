40c0fe0a7fd7deeefea2ad7c58893f45ff89b29e
Margarita Ramirez <margarita26.mr@gmail.com>
2024-05-16 15:36:54 -0400

    passed all the test

--- server/app.py
+++ server/app.py
@@ -16,11 +16,60 @@ db.init_app(app)
 
 @app.route('/messages')
 def messages():
-    return ''
+    return [message.to_dict() for message in Message.query.all()], 200
+    
 
-@app.route('/messages/<int:id>')
+@app.post('/messages')
+def post_message():
+    new_message = Message(
+        body=request.json.get('body'),
+        username=request.json.get('username')
+    )
+
+    db.session.add( new_message )
+    db.session.commit()
+
+    return new_message.to_dict(), 201
+
+@app.patch('/messages/<int:id>')
+def patch_message(id):
+    messages_update = Message.query.where(Message.id == id).first()
+
+    if messages_update:
+        for key in request.json.keys():
+            if not key == 'id':
+                setattr(messages_update, key, request.json[key])
+
+        db.session.add( messages_update )
+        db.session.commit()
+
+        return messages_update.to_dict(), 202
+    else:
+        return { 'error': "Not found" }, 404
+
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
 def messages_by_id(id):
-    return ''
+    messages = Message.query.filter(Message.id == id).first()
+
+    if request.method == 'DELETE':
+        db.session.delete(messages)
+        db.session.commit()
+
+        response_body = {
+            'delete_successful': True,
+            'message': 'Message deleted'
+        }
+
+        response = make_response(
+            response_body,
+            200
+        )
+        return response
+    
+   
+
+
 
 if __name__ == '__main__':
     app.run(port=5555)
